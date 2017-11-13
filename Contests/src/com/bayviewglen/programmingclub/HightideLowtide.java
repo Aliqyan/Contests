32439335fcb02481a00964353a8b29c5a88c61e0
Aliqyan <atapia1@bayviewglen.ca>
2017-11-12 23:27:36 -0500

    WESLEY STOP CREEPING ON MY GITHUB

--- Contests/src/com/bayviewglen/programmingclub/HightideLowtide.java
+++ Contests/src/com/bayviewglen/programmingclub/HightideLowtide.java
@@ -9,19 +9,29 @@ public class HightideLowtide {
 
 	public static void main(String[] args) throws IOException {
 		
-	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	String[] tokens = in.readLine().split(" ");
-	int n = Integer.parseInt(tokens[0]);
-	int[] nums = new int[n];
-	String[] bat = in.readLine().split(" ");
-	for(int i = 0; i< n ;i++) {
-		nums[i] = Integer.parseInt(bat[i]);
-	}
-	Arrays.sort(nums);
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String[] tokens = in.readLine().split(" ");
+		int n = Integer.parseInt(tokens[0]);
+		int[] nums = new int[n];
+		String[] bat = in.readLine().split(" ");
+		for(int i = 0; i< n ;i++) {
+			nums[i] = Integer.parseInt(bat[i]);
+		}
+		Arrays.sort(nums);
 
-	int mid = n/2;
-	for(int i = 0; i< n/2 ;i++) {
-		System.out.print(nums[mid-i-1] + " " + nums[i+mid] + " ");
-	}
+		int mid = n/2;
+		if(n%2==0){
+			for(int i = 0; i< n/2 ;i++) {
+				System.out.print(nums[mid-i-1] + " " + nums[i+mid] + " ");
+			}
+		}else{
+		    if(n > 0){
+				   System.out.print(nums[mid] + " ");
+		    }
+			for(int i = 0; i< n/2 ;i++) {
+				System.out.print(nums[mid+i+1] + " " + nums[mid-i-1] + " ");
+			}
+		}
+
 	}
 }
